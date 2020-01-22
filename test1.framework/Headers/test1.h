896debfab6d52bb80e9402cbca00e57a541ecb75
Will Jessop <willjessop@Wills-MacBook-Pro.local>
2020-01-22 16:36:07 -0500

    first

---
+++ test1.framework/Headers/test1.h
@@ -0,0 +1,19 @@
+//
+//  test1.h
+//  test1
+//
+//  Created by Will Jessop on 1/22/20.
+//  Copyright © 2020 Will Jessop. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for test1.
+FOUNDATION_EXPORT double test1VersionNumber;
+
+//! Project version string for test1.
+FOUNDATION_EXPORT const unsigned char test1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <test1/PublicHeader.h>
+
+
